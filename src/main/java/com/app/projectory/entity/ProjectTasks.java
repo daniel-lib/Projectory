a0293818bdd32a5ea93669221536f961dd7c431f
Daniel <danieltalems@gmail.com>
2023-06-28 14:29:52 +0300

    added project-task feature

---
+++ src/main/java/com/app/projectory/entity/ProjectTasks.java
@@ -0,0 +1,55 @@
+package com.app.projectory.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class ProjectTasks {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private String taskId;
+	private String taskName;
+	private String taskDescription;
+	private String status;
+	
+		
+	public ProjectTasks() {
+		super();
+	}
+
+	public ProjectTasks(String taskId, String taskName, String taskDescription) {
+		super();
+		this.taskId = taskId;
+		this.taskName = taskName;
+		this.taskDescription = taskDescription;
+	}
+
+	public String getTaskId() {
+		return taskId;
+	}
+
+	public void setTaskId(String taskId) {
+		this.taskId = taskId;
+	}
+
+	public String getTaskName() {
+		return taskName;
+	}
+
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+	public String getTaskDescription() {
+		return taskDescription;
+	}
+
+	public void setTaskDescription(String taskDescription) {
+		this.taskDescription = taskDescription;
+	}
+	
+	
+	
+
+}
