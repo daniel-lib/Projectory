e610ab2560049de10cf99f20a9b9977b8796e882
Daniel <danieltalems@gmail.com>
2023-06-29 19:56:00 +0300

    dashboard side menu added

--- src/main/resources/static/javascript/dashboardScript.js
+++ src/main/resources/static/javascript/dashboardScript.js
@@ -14,3 +14,35 @@ function closeSigninForm() {
 	//form.style.height = "0px";		
 	//form.style.display = "none";
 }
+var isCollapsed = false;
+function sideMenuToggle() {
+	
+	const sideMenu = document.querySelector("#side-menu");
+	const sideNav = document.querySelector(".side-nav");
+	const sideNavText = document.getElementsByClassName("side-nav-text");
+	
+	if (!isCollapsed) {
+		/*const sideNav = document.select*/
+		for (let t of sideNavText) {
+			t.style.display = "none";
+		}
+		/*//this two lines of code only appy padding. it ignores border change. lookup explanation
+		sideNav.style.border = "3px solid red";
+		sideNav.style = "padding-right: 1px !important";*/
+		sideNav.style = "padding-right: 1px !important";
+		isCollapsed = true;
+	}
+	else {
+		sideNav.style = "padding-right: 100px !important;";
+		for (let t of sideNavText) {
+			t.style.display = "inline";
+		}
+		isCollapsed = false;
+	}
+	//sideMenu.ariaLabel = "folded"
+
+
+	/*sideNav.style.width = "50px"
+	sideNav.style.border = "3px solid red";*/
+
+}
