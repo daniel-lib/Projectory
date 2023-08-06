371f5b3712a412777ef158b746d56b9ff701853e
Daniel <danieltalems@gmail.com>
2023-08-06 23:27:21 +0300

    minor ui tweak

--- src/main/resources/static/javascript/charts-project.js
+++ src/main/resources/static/javascript/charts-project.js
@@ -1,16 +1,20 @@
-const config = {
-  type: 'doughnut',
-  data: data,
-  options: {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-      },
-      title: {
-        display: true,
-        text: 'Chart.js Doughnut Chart'
+ const projectsChart = document.getElementById('projects-chart');
+ new Chart(projectsChart, {	  
+    type: 'pie',
+    data: {
+      labels: ['Not started', 'In progress', 'Completed', 'On Hold'],
+      datasets: [{
+        label: '# of projects',
+        data: [ 3, 5, 2, 6],
+        borderWidth: 1,
+        backgroundColor: ['#f26400', '#3fb97c', '#3487ed', '#f5a900'],
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
       }
     }
-  },
-};
+  });
