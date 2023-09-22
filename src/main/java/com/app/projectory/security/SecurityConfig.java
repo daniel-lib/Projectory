0de485af61b3584202d1f0ee8756e07f438789a8
Daniel <danieltalems@gmail.com>
2023-09-22 23:16:20 +0300

    security config continued

--- src/main/java/com/app/projectory/security/SecurityConfig.java
+++ src/main/java/com/app/projectory/security/SecurityConfig.java
@@ -1,24 +1,36 @@
 package com.app.projectory.security;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth)throws Exception{
+		auth.inMemoryAuthentication()
+		.withUser("user")
+		.password("123")
+		.roles("USER");
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
 	/*
 	 * @Bean public SecurityFilterChain filterChain(HttpSecurity http) throws
 	 * Exception { http .authorizeHttpRequests((authz) -> authz
 	 * .anyRequest().authenticated() ) .httpBasic(withDefaults()); return
 	 * http.build(); }
-	 * 
+	 * z
 	 * @Bean public WebSecurityCustomizer webSecurityCustomizer() { return (web) ->
 	 * web.ignoring().antMatchers("/", "/ignore2"); }
 	 */
