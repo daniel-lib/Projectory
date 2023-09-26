5d9e28273417b8418c9ea90fc1f43434f73dc1c6
Daniel <danieltalems@gmail.com>
2023-09-26 23:35:11 +0300

    started adding jdbc based auth

--- src/main/java/com/app/projectory/security/SecurityConfig.java
+++ src/main/java/com/app/projectory/security/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.app.projectory.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -13,17 +17,33 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	DataSource dataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth)throws Exception{
-		auth.inMemoryAuthentication()
-		.withUser("user")
-		.password("123")
-		.roles("USER");
+		auth.jdbcAuthentication().dataSource(dataSource)
+			.usersByUsernameQuery("SELECT username, password, enabled FROM users where username = ?");
+		
 	}
 	
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+			http.authorizeRequests()
+				.antMatchers("/user").hasRole("USER")
+				.antMatchers("/user/**").hasRole("USER")
+				.antMatchers("/db/**").permitAll()
+				.antMatchers("/").authenticated().and().formLogin();
+			
+			http.csrf().disable();
+			http.headers().frameOptions().disable();
+				
 	}
 	/*
 	 * @Bean public SecurityFilterChain filterChain(HttpSecurity http) throws
